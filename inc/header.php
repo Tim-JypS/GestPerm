e80af17191ab3dd12be40588592c0ac6ff4ea12c
Tim-JypS <62108503+Tim-JypS@users.noreply.github.com>
2021-11-27 13:41:33 +0000

    Add Inc Folder

---
+++ inc/header.php
@@ -0,0 +1,192 @@
+<!-- Main Banner -->
+<div class="navbar-area">
+            <!-- Menu For Mobile Device -->
+            <div class="mobile-nav">
+                <a href="index.php" class="logo">
+                    <img src="assets/img/logo.png" alt="Logo">
+                </a>
+            </div>
+
+            <!-- Menu For Desktop Device -->
+            <div class="main-nav">
+                <div class="container">
+                    <nav class="navbar navbar-expand-md navbar-light ">
+                        <a class="navbar-brand" href="index.php">
+                            <img src="assets/img/logo.png" alt="Logo">
+                        </a>
+                        <a class="navbar-brand-sticky" href="index.php">
+                            <img src="assets/img/sticky-logo.png" alt="Logo">
+                        </a>
+
+                        <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                            <ul class="navbar-nav m-auto">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link active">
+                                        Home 
+                                        <i class='bx bxs-chevron-right'></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="index.php" class="nav-link active">
+                                                Home One 
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="index-two.php" class="nav-link">
+                                                Home Two
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="index-three.php" class="nav-link">
+                                                Home Three
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="about.php" class="nav-link">
+                                        About
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        Pages 
+                                        <i class='bx bxs-chevron-right'></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="members.php" class="nav-link">
+                                                Members
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="portfolio.php" class="nav-link">
+                                                Portfolio
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="pricing.php" class="nav-link">
+                                                Pricing
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="404.php" class="nav-link">
+                                                404 Page
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="faq.php" class="nav-link">
+                                                FAQ
+                                            </a>
+                                        </li>
+                                       
+                                        <li class="nav-item">
+                                            <a href="sign-in.php" class="nav-link">
+                                                Sign In
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="sign-up.php" class="nav-link">
+                                                Sign Up
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="recover-password.php" class="nav-link">Recover Password</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="terms-condition.php" class="nav-link">
+                                                Terms & Conditions
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="privacy-policy.php" class="nav-link">
+                                                Privacy Policy
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="#" class="nav-link">
+                                                Level 1
+                                                <i class="bx bx-chevron-right"></i>
+                                            </a>
+                                            <ul class="dropdown-menu">
+                                                <li class="nav-item">
+                                                    <a href="#" class="nav-link">Level 2</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="#" class="nav-link">Level 2</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="#" class="nav-link">
+                                                        Level 2
+                                                        <i class="bx bx-chevron-right"></i>
+                                                    </a>
+                                                    <ul class="dropdown-menu">
+                                                        <li class="nav-item">
+                                                            <a href="#" class="nav-link">Level 3</a>
+                                                        </li>
+                                                        <li class="nav-item">
+                                                            <a href="#" class="nav-link">Level 3</a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        Services 
+                                        <i class='bx bxs-chevron-right'></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="services.php" class="nav-link">
+                                                Services 
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="service-details.php" class="nav-link">
+                                                Service Details 
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="testimonial.php" class="nav-link">
+                                        Testimonial
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        Blog 
+                                        <i class='bx bxs-chevron-right'></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li class="nav-item">
+                                            <a href="blog.php" class="nav-link">
+                                                Blog
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="blog-details.php" class="nav-link">
+                                                Blog Details 
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="contact.php" class="nav-link">
+                                        Contact
+                                    </a>
+                                </li>
+                            </ul>
+
+                            <div class="menu-btn">
+                                <a href="#" class="seo-btn">SEO Score</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- Main Banner End -->
---
+++ inc/preloader.php
@@ -0,0 +1,5 @@
+<!-- Start Preloader -->
+<div class="preloader">
+    <div class="preloader-wave"></div>
+</div>
+        <!-- End Preloader -->
