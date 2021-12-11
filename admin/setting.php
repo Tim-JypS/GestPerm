a3e48395ed03dc63686d06406b31cb213fc3fbc4
projetfulle <projetfulle@gmail.com>
2021-12-11 15:19:38 +0000

    push page setting
    Config page

---
+++ admin/setting.php
@@ -0,0 +1,92 @@
+<?php require 'inc/_global/config.php'; ?>
+<?php require '../inc/config.php'; ?>
+<?php require 'inc/backend/config.php'; ?>
+<?php require 'inc/_global/views/head_start.php'; ?>
+
+<!-- Page JS Plugins CSS -->
+<?php $one->get_css('js/plugins/datatables/dataTables.bootstrap4.css'); ?>
+<?php $one->get_css('js/plugins/datatables/buttons-bs4/buttons.bootstrap4.min.css'); ?>
+<?php $one->get_css('js/plugins/select2/css/select2.min.css'); ?>
+
+<!-- Page JS Plugins CSS -->
+<?php $one->get_css('js/plugins/sweetalert2/sweetalert2.min.css'); ?>
+
+<?php require 'inc/_global/views/head_end.php'; ?>
+<?php require 'inc/_global/views/page_start.php'; ?>
+
+<!-- Hero -->
+<div class="bg-body-light">
+    <div class="content content-full">
+        <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
+            <!-- <h1 class="flex-sm-fill h3 my-2">
+                Etablissement <small class="d-block d-sm-inline-block mt-2 mt-sm-0 font-size-base font-w400 text-muted"></small>
+            </h1> -->
+            <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb-alt">
+                    <li class="breadcrumb-item">Gestion</li>
+                    <li class="breadcrumb-item" aria-current="page">
+                        <a class="link-fx" href="">Setting</a>
+                    </li>
+                </ol>
+            </nav>
+        </div>
+   </div>
+</div>
+<!-- END Hero -->
+
+<!-- Page Content -->
+<div class="content">
+    <!-- Dynamic Table Full -->
+    <div class="block block-rounded">
+        <div class="block-header">
+            <h3 class="block-title">Page des configurations <small</small></h3>
+        </div>
+        <div class="block-content block-content-full">
+            <!-- DataTables init on table by adding .js-dataTable-full class, functionality is initialized in js/pages/be_tables_datatables.min.js which was auto compiled from _js/pages/be_tables_datatables.js -->
+            <div class="row push">
+                <div class="col-lg-4">
+                    <p class="font-size-sm text-muted">
+                        <span style="color:blue; font-size:20px;">Validation électronique</span><br>
+                        <small>
+                        Pour la validation électronique, les agents doivent importer leurs différentes signatures.
+                    </small>
+                    </p>
+                </div>
+                <div class="col-lg-8 col-xl-5">
+                    <div class="form-group">
+                        <div class="custom-control custom-switch custom-control-lg mb-2">
+                            <label>NON</label>
+                            <input type="checkbox" class="custom-control-input" id="example-sw-custom-lg1" name="example-sw-custom-lg1" checked>
+                            <label class="custom-control-label" for="example-sw-custom-lg1">OUI</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <!-- END Dynamic Table Full -->
+    
+
+<!-- END Page Content -->
+
+<?php require 'inc/_global/views/page_end.php'; ?>
+<?php require 'inc/_global/views/footer_start.php'; ?>
+
+<!-- Page JS Plugins -->
+<?php $one->get_js('js/plugins/datatables/jquery.dataTables.min.js'); ?>
+<?php $one->get_js('js/plugins/datatables/dataTables.bootstrap4.min.js'); ?>
+<?php $one->get_js('js/plugins/datatables/buttons/dataTables.buttons.min.js'); ?>
+<?php $one->get_js('js/plugins/datatables/buttons/buttons.print.min.js'); ?>
+<?php $one->get_js('js/plugins/datatables/buttons/buttons.html5.min.js'); ?>
+<?php $one->get_js('js/plugins/datatables/buttons/buttons.flash.min.js'); ?>
+<?php $one->get_js('js/plugins/datatables/buttons/buttons.colVis.min.js'); ?>
+<?php $one->get_js('js/plugins/select2/js/select2.full.min.js'); ?>
+<?php $one->get_js('js/plugins/jquery-validation/jquery.validate.min.js'); ?>
+<?php $one->get_js('js/plugins/jquery-validation/additional-methods.js'); ?>
+
+<!-- Page JS Helpers (Select2 plugin) -->
+<script>jQuery(function(){ One.helpers('select2'); });</script>
+
+<!-- Page JS Code -->
+<?php $one->get_js('js/pages/be_tables_datatables.min.js'); ?>
+
+<?php require 'inc/_global/views/footer_end.php'; ?>
