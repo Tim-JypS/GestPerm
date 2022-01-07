5ba05dc3ff7e2dafb4fd7071dd06b225f2410e9e
Tim-JypS <62108503+Tim-JypS@users.noreply.github.com>
2022-01-07 02:30:06 +0000

    Update index.php

--- admin/print/index.php
+++ admin/print/index.php
@@ -14,6 +14,8 @@ require_once "stimulsoft/helper.php";
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>GestPerm - Fiche de permutation</title>
 	<link rel="stylesheet" type="text/css" href="css/stimulsoft.viewer.office2013.whiteblue.css">
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href="../../assets/img/favicon.png">
 	<script type="text/javascript" src="scripts/stimulsoft.reports.js"></script>
 	<script type="text/javascript" src="scripts/stimulsoft.reports.maps.js"></script>
 	<script type="text/javascript" src="scripts/stimulsoft.viewer.js"></script>
